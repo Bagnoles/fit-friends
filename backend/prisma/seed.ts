e6ec8a23385115080e9b28598118bc6eb44f28ae
Anna <anna.mironova89@yandex.ru>
2024-09-26 12:52:17 +0300

    Add DB seeding

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,190 @@
+import {
+  Gender,
+  Level,
+  PrismaClient,
+  Subway,
+  Time,
+  Type,
+} from '@prisma/client';
+
+const USERS_UUIDS = [
+  '6d308040-96a2-4162-bea6-2338e9976540',
+  '6d308040-96a2-4162-bea6-23asw5976540',
+  '6d308040-hf6s-4162-bea6-2338e9976540',
+  '6d336740-96a2-4162-bea6-23asw5976540',
+  '6d30d530-hf6s-4162-bea6-2338e9976540',
+];
+const WORKOUTS_UUIDS = [
+  '6d308040-hf6s-4162-bea6-2338e9sd2760',
+  '6d336740-96a2-4162-bea6-23asw5976hd6',
+  '6d30d530-hf6s-4162-bea6-23klye997650',
+];
+
+function getUsers() {
+  return [
+    {
+      id: USERS_UUIDS[0],
+      name: 'Alina',
+      email: 'test@test.ru',
+      avatarUrl: 'img/content/user-photo-1.png',
+      password: 'test123',
+      gender: 'Female',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+      subway: 'Pionerskaya',
+    },
+    {
+      id: USERS_UUIDS[1],
+      name: 'Elena',
+      email: 'test1@test.ru',
+      avatarUrl: 'img/content/user-photo-1.png',
+      password: 'test123',
+      gender: 'Female',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+      subway: 'Petrogradskaya',
+    },
+    {
+      id: USERS_UUIDS[2],
+      name: 'Vladimir',
+      email: 'test2@test.ru',
+      avatarUrl: 'img/content/user-photo-2.png',
+      password: 'test123',
+      gender: 'Male',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+      subway: 'Udelnaya',
+    },
+    {
+      id: USERS_UUIDS[3],
+      name: 'Oksana',
+      email: 'test3@test.ru',
+      avatarUrl: 'img/content/user-photo-1.png',
+      password: 'test123',
+      gender: 'Female',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+      subway: 'Zvezdnaya',
+    },
+    {
+      id: USERS_UUIDS[4],
+      name: 'Ruslan',
+      email: 'test4@test.ru',
+      avatarUrl: 'img/content/user-photo-2.png',
+      password: 'test123',
+      gender: 'Male',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+      subway: 'Sportivnaya',
+    },
+  ];
+}
+
+function getWorkouts() {
+  return [
+    {
+      id: WORKOUTS_UUIDS[0],
+      name: 'Тренировка 1',
+      imageUrl: 'img/content/thumbnails/training-12.jpg',
+      level: 'Beginner',
+      type: 'Running',
+      duration: 'Short',
+      price: 1500,
+      calories: 300,
+      description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+      gender: 'Female',
+      videoUrl: 'img/content/training-video/video-thumbnail.png',
+      coach: 'Nastya',
+      isSpecial: false,
+    },
+    {
+      id: WORKOUTS_UUIDS[1],
+      name: 'Тренировка 2',
+      imageUrl: 'img/content/thumbnails/training-12.jpg',
+      level: 'Professional',
+      type: 'Crossfit',
+      duration: 'VeryLong',
+      price: 3500,
+      calories: 800,
+      description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+      gender: 'Male',
+      videoUrl: 'img/content/training-video/video-thumbnail.png',
+      coach: 'Alex',
+      isSpecial: false,
+    },
+    {
+      id: WORKOUTS_UUIDS[2],
+      name: 'Тренировка 3',
+      imageUrl: 'img/content/thumbnails/training-12.jpg',
+      level: 'Amateur',
+      type: 'Aerobics',
+      duration: 'Medium',
+      price: 2000,
+      calories: 400,
+      description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+      gender: 'Female',
+      videoUrl: 'img/content/training-video/video-thumbnail.png',
+      coach: 'Nastya',
+      isSpecial: false,
+    },
+  ];
+}
+
+async function seedDb(prismaClient: PrismaClient) {
+  const mockUsers = getUsers();
+  for (const user of mockUsers) {
+    await prismaClient.user.upsert({
+      where: { email: user.email },
+      update: {},
+      create: {
+        id: user.id,
+        description: user.description,
+        email: user.email,
+        gender: user.gender as Gender,
+        imageUrl: user.avatarUrl,
+        avatarUrl: user.avatarUrl,
+        name: user.name,
+        subway: user.subway as Subway,
+        passwordHash: user.password,
+      },
+    });
+  }
+  const mockWorkouts = getWorkouts();
+  for (const workout of mockWorkouts) {
+    await prismaClient.workout.upsert({
+      where: { id: workout.id },
+      update: {},
+      create: {
+        id: workout.id,
+        calories: workout.calories,
+        coach: workout.coach,
+        description: workout.description,
+        duration: workout.duration as Time,
+        gender: workout.gender as Gender,
+        imageUrl: workout.imageUrl,
+        isSpecial: workout.isSpecial,
+        level: workout.level as Level,
+        name: workout.name,
+        price: workout.price,
+        type: workout.type as Type,
+        videoUrl: workout.videoUrl,
+      },
+    });
+  }
+}
+
+async function bootstrap() {
+  const prismaClient = new PrismaClient();
+
+  try {
+    await seedDb(prismaClient);
+    globalThis.process.exit(0);
+  } catch (error: unknown) {
+    console.error(error);
+    globalThis.process.exit(1);
+  } finally {
+    await prismaClient.$disconnect();
+  }
+}
+
+bootstrap();
